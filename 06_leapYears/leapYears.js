ae6e16be5011a9869a48ddc46b384f9a7ffcb61a
Patrick <taaaallen@gmail.com>
2023-04-09 20:58:50 +0200

    More solutions added

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,6 @@
-const leapYears = function (year) {};
+const leapYears = function (year) {
+  return year % 4 == 0 && (year % 400 == 0 || year % 100 != 0);
+};
 
 // Do not edit below this line
 module.exports = leapYears;
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const regex = /[^A-Za-z0-9]|\s|,/g;
+  const strippedString = string.toLowerCase().replace(regex, "");
+  const reverseString = string.split("").reverse().join("").toLowerCase().replace(regex, "");
 
+  //   console.log(reverseString);
+  //   console.log(strippedString);
+  return reverseString == strippedString ? true : false;
 };
+
+// palindromes("A car, a man, a maraca.");
 
 // Do not edit below this line
 module.exports = palindromes;
