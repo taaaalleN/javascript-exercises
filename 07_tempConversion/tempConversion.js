c38e8d40d8224580ab17824ba1b68490801023d7
Patrick <taaaallen@gmail.com>
2023-04-10 20:37:09 +0200

    More solutions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const convertedTemp = (temp - 32) * (5 / 9);
+  return convertedTemp == 0 ? convertedTemp : round(convertedTemp, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const convertedTemp = (temp * (9 / 5) + 32).toFixed(1);
+  return convertedTemp == 0 ? convertedTemp : round(convertedTemp, 1);
 };
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
